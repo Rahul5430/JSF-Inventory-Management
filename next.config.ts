0b848868d604d704aad4d328c58c806f373cd19f
Rahul Sharma <67220225+Rahul5430@users.noreply.github.com>
2025-07-07 16:09:06 +0530

    pwa testing

--- next.config.ts
+++ next.config.ts
@@ -19,13 +19,6 @@ const nextConfig: NextConfig = {
 	images: {
 		domains: ['firebasestorage.googleapis.com'],
 	},
-	webpack: (config) => {
-		config.module.rules.push({
-			test: /\.svg$/,
-			use: ['@svgr/webpack'],
-		});
-		return config;
-	},
 	eslint: {
 		ignoreDuringBuilds: true,
 	},
